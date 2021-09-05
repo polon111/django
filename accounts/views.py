383fe8ab0b1731ed98df5e5b67fcf015a43046f8
Grzegorz <gzx@wp.pl>
2021-09-05 12:15:10 +0200

    i zmiany

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.urls import reverse_lazy
 
 
 class SubmittableLoginView(LoginView):
     template_name = 'form.html'
+
+
+class SubmittablePasswordChangeForm(PasswordChangeView):
+    template_name = 'form.html'
+    success_url = reverse_lazy('index')
